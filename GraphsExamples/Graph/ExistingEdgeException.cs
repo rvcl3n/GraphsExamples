0df18c4991cd4ae2614771d25e8c4c33eb426d3c
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-04 09:37:35 -0700

    Added a custom Exception

---
+++ GraphsExamples/Graph/ExistingEdgeException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GraphsExamples
+{
+    public class ExistingEdgeException : Exception
+    {
+        public ExistingEdgeException()
+        {
+
+        }
+
+        public ExistingEdgeException(string message)
+            :base(message)
+        {
+
+        }
+
+        public ExistingEdgeException(string message, Exception inner)
+            :base(message, inner)
+        {
+
+        }
+    }
+}
