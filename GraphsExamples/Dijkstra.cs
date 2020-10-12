9f37088afa5c4b39d30ae03140a85444e4e69525
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-12 23:33:34 +0300

    Removed a previous version of the Dijkstra algorithm

--- GraphsExamples/Dijkstra.cs
+++ GraphsExamples/Dijkstra.cs
@@ -5,59 +5,6 @@ namespace GraphsExamples
 {
     class Dijkstra
     {
-        int V = 0;
-        int minDistance(int[] dist,
-                        bool[] sptSet)
-        {
-            int min = int.MaxValue, min_index = -1;
-
-            for (int v = 0; v < V; v++)
-                if (sptSet[v] == false && dist[v] <= min)
-                {
-                    min = dist[v];
-                    min_index = v;
-                }
-
-            return min_index;
-        }
-
-        void printSolution(int[] dist)
-        {
-            Console.Write("Vertex \t\t Distance "
-                          + "from Source\n");
-            for (int i = 0; i < V; i++)
-                Console.Write(i + " \t\t " + dist[i] + "\n");
-        }
-
-        public void dijkstra(int[,] graph, int src)
-        {
-            V = graph.GetLength(0);
-
-            int[] dist = new int[V];
-            bool[] sptSet = new bool[V];
-
-            for (int i = 0; i < V; i++)
-            {
-                dist[i] = int.MaxValue;
-                sptSet[i] = false;
-            }
-
-            dist[src] = 0;
-
-            for (int count = 0; count < V - 1; count++)
-            {
-                int u = minDistance(dist, sptSet);
-
-                sptSet[u] = true;
-
-                for (int v = 0; v < V; v++)
-                    if (!sptSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
-                        dist[v] = dist[u] + graph[u, v];
-            }
-
-            printSolution(dist);
-        }
-
         public static void DijkstraFunc(Graph graph, Node start)
         {
             //arrays init
